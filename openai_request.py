75c0c9e7c56ef42f87dab5128119ef1b9d08ff41
--global <--global>
2023-11-10 10:15:46 +0900

    openai

--- openai_request.py
+++ openai_request.py
@@ -1,5 +1,8 @@
+import os
 from openai import OpenAI
-client = OpenAI()
+client = OpenAI(
+  api_key=os.environ.get(".key")
+)
 
 completion = client.chat.completions.create(
   model="gpt-3.5-turbo",
